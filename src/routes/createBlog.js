a3dd93bccef279060d6ef6abc66eaab67b64e5ba
rinbk201 <yamarin8zoo@gmail.com>
2023-06-07 17:40:00 +0900

    createBlog

--- src/routes/createBlog.js
+++ src/routes/createBlog.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 function CreateBlog() {
   const [title,setTitle] = useState("");
   const [contents,setContents] = useState("");
-  
+
   return (
     <div style={{ margin: "auto", width: "1000px" }}>
       <h1>ブログ作成画面</h1>
@@ -13,6 +13,22 @@ function CreateBlog() {
       </div>
       <div>
         <Link to="/blogs">記事一覧画面</Link>
+      </div>
+      <h2>タイトルを入力してください</h2>
+      <div>
+        <input
+          value={title}
+          onChange={(e)=> setTitle(e.target.value)}
+          style={{width:"500px"}}
+        />  
+      </div>
+      <h2>本文を入力してください</h2>
+      <div>
+        <textarea
+          value={contents}
+          onChange={(e)=> setContents(e.target.value)}
+          style={{width:"500px",height:"500px"}}
+        />
       </div>
     </div>
   );
